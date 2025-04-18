5fcba9f2f80cfbaeb56777655cb2e1aa3ecd155d
ChauGiaBao2812 <giabao28122004@gmail.com>
2025-04-18 19:17:03 +0700

    Push again

--- ChartControls/Pages/PieChart.cshtml.cs
+++ ChartControls/Pages/PieChart.cshtml.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ChartControls.Models; // namespace chứa SharedData
 
 namespace ChartControls.Pages
 {
